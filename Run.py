1d4f00616b3eaf18f39cb6f5d97682b000ddca13
wootiown <46952165+wootiown@users.noreply.github.com>
2019-01-24 00:39:18 -0500

    Add files via upload

--- Run.py
+++ Run.py
@@ -3,12 +3,8 @@ from Initialize import joinRoom
 from SongRequest import *
 import string
 import vlc
+from threading import Thread
 
-s = openSocket()
-joinRoom(s)
-readbuffer = ""
-pingActive = 0
-data = ""
 
 
 
@@ -28,39 +24,116 @@ def getMessage(line):
 def send_pong(msg):
     s.send(bytes('PONG %s\r\n' % msg, 'UTF-8'))
 
+p = vlc.MediaPlayer()
 
 
-while True:
-    try:
-        readbuffer = readbuffer + s.recv(1024)
-        temp = string.split(readbuffer, "\n")
-        readbuffer = temp.pop()
 
-        for line in temp:
+def main():
+    import time
+    s = openSocket()
+    joinRoom(s)
+    readbuffer = ""
+    pingActive = 0
+    data = ""
+    timecontrol = time
 
-            if "PING" in line:
-                s.send("PONG %s\r\n" % line[1])
-                print("Send a PONG to Twitch's servers")
-            else:
-
-                user = getUser(line)
-                message = str(getMessage(line))
-
-                print(">> " + user + ": " + message)
-
-                if "!sr" in message:
-                    song_name = message.replace("!sr", '')
-                    cmd_test(song_name)
-                if "!pause" in message:
-                    cmd_pause()
+    while True:
+            try:
+                readbuffer = readbuffer + s.recv(1024)
+                temp = string.split(readbuffer, "\n")
+                readbuffer = temp.pop()
 
 
 
 
+                for line in temp:
+
+                    if "PING" in line:
+                        s.send("PONG %s\r\n" % line[1])
+                        print("Send a PONG to Twitch's servers")
+                    else:
+
+                        user = getUser(line)
+                        message = str(getMessage(line))
+
+                        print(">> " + user + ": " + message)
+
+                        if "!sr" in message.lower():
+                            song_name = message.replace("!sr", '')
+                            url = cmd_sr(song_name)
+                            global p
+                            p = vlc.MediaPlayer(url)
+                            p.play()
+                            timecontrol.sleep(1.5) # startup time.
+                            duration = p.get_length() / 1000
+                            mm, ss   = divmod(duration, 60)
+                            print "Current song length is: ", "%02d:%02d" % (mm,ss)
+
+                            global nowplaying
+                            nowplaying = True
+
+                            t2.start()
+
+                        if "!pause" in message.lower():
+                            p.pause()
+                            nowplaying = False
+                        if "!play" in message.lower():
+                            p.pause()
+                            nowplaying = True
+                        if "!time" in message.lower():
+                            time = p.get_time()
+                            sendMessage(s, str(time))
 
 
-    except socket.error:
-        print("Socket died")
+                        if "!volume" in message.lower():
+                            vol = message.replace("!volume", '')
+                            vol = int(vol.replace("/r", ''))
+                            if vol > 100 or vol < 0:
+                                sendMessage(s, "Invalid volume level. Must be between 0-100.")
+                                break
 
-    except socket.timeout:
-        print("Socket timeout")
+                            p.audio_set_volume(vol)
+                            sendMessage(s, "Music volume set to: " + str(vol))
+                        #if "!volumeup" in message.lower():
+                        #    amt = int(message.replace("!volumeup", ''), message.replace("/r", ''))
+                        #    print(str(amt))
+                        #    volume = p.audio_get_volume
+                        #    p.pause()
+
+                        #if "!volumedown" in message.lower():
+                        #    pass
+
+
+
+
+
+
+
+
+
+            except socket.error:
+                print("Socket died")
+
+            except socket.timeout:
+                print("Socket timeout")
+
+def tick():
+    import time
+    print("ping")
+    while True:
+        if nowplaying == True:
+            nptime = int(p.get_time())
+            nplength = int(p.get_length())
+
+            if (nptime + 1500) > nplength:
+                time.sleep(1.5)
+                sendMessage(s, "Song is over!")
+                global nowplaying
+                nowplaying = False
+
+
+t1 = Thread(target = main)
+t2 = Thread(target = tick)
+
+
+t1.start()
--- SongRequest.py
+++ SongRequest.py
@@ -3,6 +3,10 @@ from gmusicapi import Mobileclient
 from Settings import GMEmail, GMPass
 import vlc
 
+
+
+
+
 s = openSocket()
 api = Mobileclient()
 api.login(GMEmail, GMPass, Mobileclient.FROM_MAC_ADDRESS)
@@ -11,27 +15,29 @@ if Mobileclient.is_authenticated(api) == True:
     print("Logged into GPM successfully")
 else:
     print("Can't log into Google Play Music.")
+stream_url = ""
 
-def cmd_test(song_name):
+def cmd_sr(song_name):
     print(song_name)
 
-    #try:
-    results = Mobileclient.search(api, song_name)
-    top_song_result = results['song_hits'][0]['track']
+    try:
+        results = Mobileclient.search(api, song_name)
+        top_song_result = results['song_hits'][0]['track']
 
-    sendMessage(s, "I found this song: " + str(top_song_result['artist'] + " - " + top_song_result['title']))
+        sendMessage(s, "I found this song: " + str(top_song_result['artist'] + " - " + top_song_result['title']))
+        global stream_url
+        stream_url = Mobileclient.get_stream_url(api, top_song_result['storeId'], "3e9ff840362801d4")
+        return(stream_url)
 
-    stream_url = Mobileclient.get_stream_url(api, top_song_result['storeId'], "3e9ff840362801d4")
-
-
-    p = vlc.MediaPlayer(stream_url)
-    p.play()
-
+    except IndexError:
+        sendMessage(s, "No results found for that song. Please try a different one.")
 
 def cmd_pause():
-    p = vlc.MediaPlayer()
+    print(stream_url)
+    p = vlc.MediaPlayer(stream_url)
+    p.play()
     sendMessage(s, "Tried to pause")
-    p.stop()
+    p.pause()
     #subprocess.call(['mpv', stream_url])
     #except:
         #sendMessage(s, "Error. Either no results found or shit broke.")
