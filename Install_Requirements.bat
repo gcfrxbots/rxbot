5520fcca398405e75917cafacb957ca5cee4cdac
Grant Fowler <gcf.rxbots@gmail.com>
2019-07-22 22:49:33 -0400

    Update Install_Requirements.bat

--- Install_Requirements.bat
+++ Install_Requirements.bat
@@ -1,4 +1,4 @@
-echo ================================================================= & echo.You need to have Python 3.7 (and the included PIP package) installed for this to work! & echo.THIS NEEDS TO BE RUN AS ADMINISTRATOR TO INSTALL PACKAGES!
+echo ================================================================= & echo.You need to have Python 3.7 (and the included PIP package) installed for this to work!
 pause
 
 py -3.7 -m pip install -r requirements.txt --user --no-warn-script-location
